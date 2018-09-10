8fd5349c2d683a74a3f12b2781b7e6971ab3b77d
Kaleb Spillman <kalebspillman@Interapts-MacBook-Pro-5.local>
2018-09-10 13:16:11 -0500

    initial commit

---
+++ VideoGameLibrary/VideoGameLibrary/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VideoGameLibrary
+//
+//  Created by Kaleb Spillman on 9/10/18.
+//  Copyright © 2018 Kaleb Spillman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
